bcd37e46b4c400976952ac4324f72846684de544
Matt Blair <mblair@slack-corp.com>
2021-05-07 18:57:55 -0700

    update deps

--- .labrc.js
+++ .labrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   lint: true,
-  reporter: ['console', 'html', 'lcov', 'json'],
-  output: ['stdout', 'coverage/coverage.html', 'coverage/lcov.info', 'coverage/data.json'],
-  pattern: "spec"
+  reporter: ["console", "html", "lcov", "json"],
+  output: [
+    "stdout",
+    "coverage/coverage.html",
+    "coverage/lcov.info",
+    "coverage/data.json",
+  ],
+  pattern: "spec",
+  globals: "globalThis"
 };
